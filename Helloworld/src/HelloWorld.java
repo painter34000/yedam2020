d917e916c174e9ba2e90410210fd28df89615d25
User <User@YD02-02>
2020-01-21 12:37:24 +0900

    2020

---
+++ Helloworld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		int age = 30;
+		int year = 2020;
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("Hello World");
+		System.out.println("올해는" + year);
+		System.out.println("나이는" + age);
+		System.out.println(age + year);
+		System.out.println("내나이는"+ age +"입니다");
+	}
+}
+// 여기는 실행이 안됩니다. Hello World를 출력하는 구문
+
+/*
+ * 이 안의 내용은 모든것이 주석처리가 됩니다. 작성자 : 2020 홍성우 여러줄을 주석을 달때 사용
+ */
