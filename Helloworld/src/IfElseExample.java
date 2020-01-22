1a74317131b7c721fcc65c892595ff28b08ac244
User <User@YD02-02>
2020-01-22 17:55:04 +0900

    0122 저녁

---
+++ Helloworld/src/IfElseExample.java
@@ -0,0 +1,40 @@
+
+public class IfElseExample {
+	public static void main(String[] args) {
+
+		int result = 55;
+		if (result >= 90) {
+			if (result >= 95) {
+				System.out.println("A+ 학점");
+			} else {
+				System.out.println("A 학점");
+			}
+
+		} else if (result >= 80) {
+			if (result >= 85) {
+				System.out.println("B+ 학점");
+			} else {
+				System.out.println("B 학점");
+			}
+
+		} else if (result >= 70) {
+
+			if (result >= 75) {
+				System.out.println("C+ 학점");
+			} else {
+				System.out.println("C 학점");
+			}
+
+		} else if (result >= 60) {
+
+			if (result >= 75) {
+				System.out.println("D+ 학점");
+			} else {
+				System.out.println("D 학점");
+			}
+
+		} else {
+			System.out.println("F 학점");
+		}
+	}
+}
---
+++ Helloworld/src/Variabletest.java
@@ -0,0 +1,20 @@
+
+public class Variabletest {
+	public static void main(String[] args) {
+
+		int sum, num1, num2;
+		double avg;
+		num1 = 30;
+		num2 = 45;
+		sum = num1 + num2;
+		avg = (double) (num1 + num2) / 2;
+
+		System.out.println("두수 num1과 num2의 합은 :" + sum);
+		System.out.println("두수 num1과 num2의 평균은 :" + avg);
+		
+		
+				
+		
+	}
+
+}
