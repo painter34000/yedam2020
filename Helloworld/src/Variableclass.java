c35e241c05ada7f0d6b2c3e317b7f6d20466d2c1
User <User@YD02-02>
2020-01-22 10:40:11 +0900

    0122연습

---
+++ Helloworld/src/Variableclass.java
@@ -0,0 +1,16 @@
+
+public class Variableclass {
+	public static void main(String[] args) {
+
+		int pum, sum, num1, num2, num3;
+		num1 = 100;
+		num2 = 90;
+		num3 = 80;
+		sum = num1 + num2 + num3;
+		pum = num1 + num3;
+
+		System.out.println("sum의 값은: " + sum);
+		System.out.println("pum의 값은:" + pum);
+
+	}
+}
